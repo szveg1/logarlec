ea9d3b41e0b976aa59797dd7bdca98d72f47c8f8
Ábel András Attila <abel.andras.attila1@gmail.com>
2024-03-22 13:05:27 +0100

    .

--- logarlec_java/src/main/java/pass/logarlec/Main.java
+++ logarlec_java/src/main/java/pass/logarlec/Main.java
@@ -10,6 +10,6 @@ import pass.logarlec.labyrinth.Szoba;
 public class Main {
     static Labirintus labirintus = new Labirintus();
     public static void main(String[] args) {
-
+        System.out.println("Hello world!");
     }
 }
