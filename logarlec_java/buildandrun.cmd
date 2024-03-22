347170d6ac257b44a78be9516478b78552e96a73
Ábel András <119449017+szveg1@users.noreply.github.com>
2024-03-22 13:10:11 +0100

    Update buildandrun.cmd

--- logarlec_java/buildandrun.cmd
+++ logarlec_java/buildandrun.cmd
@@ -1,7 +1,17 @@
 @echo off
-rem Find and set JAVA_HOME
-call setjavahome.cmd
+setlocal
 
+rem Check if JAVA_HOME is already set
+if "%JAVA_HOME%"=="" (
+    rem If JAVA_HOME is not set, call the script to find it
+    call find_java.cmd
+) else (
+    echo JAVA_HOME is already set to: %JAVA_HOME%
+)
+
+rem Run other commands here...
+
+endlocal
 rem Make .jar file
 call mvnw.cmd clean install
 
