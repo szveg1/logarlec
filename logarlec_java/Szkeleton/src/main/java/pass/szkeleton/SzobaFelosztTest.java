3866774148e9bc7bc02264cd6d6d855f80707f07
Lévai Márton <marci.levai@gmail.com>
2024-03-24 16:18:38 +0100

    feloszt

--- logarlec_java/Szkeleton/src/main/java/pass/szkeleton/SzobaFelosztTest.java
+++ logarlec_java/Szkeleton/src/main/java/pass/szkeleton/SzobaFelosztTest.java
@@ -42,10 +42,10 @@ public class SzobaFelosztTest {
         if (labirintus.getSzobak().get(0).getFerohely() == labirintus.getSzobak().get(1).getFerohely()) Szkeleton.logger.info("Letrejott szoba ferohelye egyenlo a regivel");
         else Szkeleton.logger.log(Level.WARNING, "Letrejott szoba ferohelye nem egyenlo a regivel");
 
-        if (labirintus.getSzobak().get(0).getEmberek().size() - labirintus.getSzobak().get(1).getEmberek().size() <= 1) Szkeleton.logger.info("Letrejott szobaba az emberek fele kerult");
+        if (Math.abs(labirintus.getSzobak().get(0).getEmberek().size() - labirintus.getSzobak().get(1).getEmberek().size()) <= 1) Szkeleton.logger.info("Letrejott szobaba az emberek fele kerult");
         else Szkeleton.logger.log(Level.WARNING, "Letrejott szobaba baj van az emberek szamaval");
 
-        if (labirintus.getSzobak().get(0).getItems().size() - labirintus.getSzobak().get(1).getItems().size() <= 1) Szkeleton.logger.info("Letrejott szobába a targyak fele kerult");
+        if (Math.abs(labirintus.getSzobak().get(0).getItems().size() - labirintus.getSzobak().get(1).getItems().size()) <= 1) Szkeleton.logger.info("Letrejott szobába a targyak fele kerult");
         else Szkeleton.logger.log(Level.WARNING, "Letrejött szobaba baj van az targyak szamaval");
 
 
