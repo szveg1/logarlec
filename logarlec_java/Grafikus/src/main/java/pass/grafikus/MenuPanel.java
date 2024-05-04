9b65548e51574ab63727e3d22a1d12de8079283e
Ábel András Attila <abel.andras.attila1@gmail.com>
2024-05-04 13:49:38 +0200

    .

--- logarlec_java/Grafikus/src/main/java/pass/grafikus/MenuPanel.java
+++ logarlec_java/Grafikus/src/main/java/pass/grafikus/MenuPanel.java
@@ -7,8 +7,42 @@ import java.awt.*;
 
 public class MenuPanel extends JPanel {
     public MenuPanel(){
-        setLayout(new BorderLayout());
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
+        final JPanel playPanel = makePlayPanel();
+        final JPanel loadPanel = makeLoadPanel();
+
+        add(playPanel);
+        add(loadPanel);
+    }
+
+    private JPanel makeLoadPanel() {
+        JPanel loadPanel = new JPanel();
+        loadPanel.setLayout(new FlowLayout());
+        JButton loadButton = new JButton("Load");
+        loadPanel.add(loadButton);
+
+        loadButton.addActionListener(e -> {
+            loadDialog();
+            Main.setDisplayedFrame(new GameFrame());
+        });
+
+        return loadPanel;
+    }
+
+    private void loadDialog() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Load Game");
+
+        int userSelection = fileChooser.showOpenDialog(this);
+
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            Controller.Load(fileChooser.getSelectedFile().getAbsolutePath());
+            Main.setDisplayedFrame(new GameFrame());
+        }
+    }
+
+    private static JPanel makePlayPanel() {
         JPanel playPanel = new JPanel();
         playPanel.setLayout(new FlowLayout());
         JButton playButton = new JButton("Play");
@@ -24,7 +58,6 @@ public class MenuPanel extends JPanel {
             Controller.Play(playerNumber);
             Main.setDisplayedFrame(new GameFrame());
         });
-
-        add(playPanel, BorderLayout.CENTER);
+        return playPanel;
     }
 }
