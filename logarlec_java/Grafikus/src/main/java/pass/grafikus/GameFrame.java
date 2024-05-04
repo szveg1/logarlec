082e9cb912007ae12e1331c90aaffceaeadd249d
Melegh Domonkos Márton <meleghdomi@gmail.com>
2024-05-04 13:36:21 +0200

    JPopupMenu and Exit implemented

--- logarlec_java/Grafikus/src/main/java/pass/grafikus/GameFrame.java
+++ logarlec_java/Grafikus/src/main/java/pass/grafikus/GameFrame.java
@@ -1,10 +1,13 @@
 package pass.grafikus;
 
+import pass.controller.Controller;
 import pass.model.labyrinth.Labirintus;
 import pass.model.labyrinth.Szoba;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 public class GameFrame extends JFrame {
     public GameFrame() {
@@ -15,6 +18,38 @@ public class GameFrame extends JFrame {
         setLocationRelativeTo(null);
 
         setLayout(new BorderLayout());
+
+        JPopupMenu popupMenu = new JPopupMenu();
+        JMenuItem playItem = new JMenuItem("Play");
+        JMenuItem saveItem = new JMenuItem("Save Game");
+        JMenuItem loadItem = new JMenuItem("Load Game");
+        JMenuItem exitItem = new JMenuItem("Exit Game");
+        exitItem.addActionListener(e -> {
+            Controller.reset();
+            Main.setDisplayedFrame(new MenuFrame());
+        });
+        popupMenu.add(playItem);
+        popupMenu.add(saveItem);
+        popupMenu.add(loadItem);
+        popupMenu.add(exitItem);
+
+        Action escapeAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (popupMenu.isVisible()) {
+                    popupMenu.setVisible(false);
+                } else {
+                    popupMenu.show(GameFrame.this, 8, 31);
+                }
+            }
+        };
+
+        ((JComponent) GameFrame.this.getContentPane()).getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "ESCAPE");
+        ((JComponent) GameFrame.this.getContentPane()).getActionMap().put("ESCAPE", escapeAction);
+
+        GameFrame.this.setFocusable(true);
+
+
 
         add(new SzobaPanel(Labirintus.getInstance().getSzobak().get(0), getSize()), BorderLayout.CENTER);
         add(new JTextArea("Ez egy teszt szöveg"), BorderLayout.SOUTH);
